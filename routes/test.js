7abf66d9907a7bd13b1c4da960cc15c076815503
brightmo <kyeongmo2@gmail.com>
2016-04-05 23:38:09 +0900

    Add routes/test.js

---
+++ routes/test.js
@@ -0,0 +1,16 @@
+/*
+ * GET login page.
+ */
+
+exports.login = function(req, res, next) {
+  res.render('login');
+};
+
+/*
+ * GET logout route.
+ */
+
+exports.logout = function(req, res, next) {
+  // req.session.destroy();
+  res.redirect('/'); //root -> index.jade
+};
