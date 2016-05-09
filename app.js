94ef17a2f538e528542ef2bee6314cc5d38be559
brightmo <kyeongmo2@gmail.com>
2016-05-09 23:11:51 +0900

    Update server app

--- app.js
+++ app.js
@@ -1,5 +1,5 @@
 var express = require('express'),
-  routes = require('./routes'),
+  // routes = require('./routes'),
   http = require('http'),
   path = require('path');
   // mongoose = require('mongoose'),
@@ -17,16 +17,16 @@ var express = require('express'),
 var app = express();
 app.locals.appTitle = "personal-web";
 
-// app.use(function(req, res, next) {
+// app.use(function(req, res, next) { //몽고디비 유효성 검사
 //   if (!models.Article || ! models.User) return next(new Error("No models."))
 //   req.models = models;
 //   return next();
 // });
 
 // All environments
-app.set('port', process.env.PORT || 8000);
-app.set('views', path.join(__dirname, 'views'));
-app.set('view engine', 'jade');
+app.set('port', process.env.PORT || 3000);
+// app.set('views', path.join(__dirname, 'views'));
+// app.set('view engine', 'jade'); //jade는 나중에 쓰도록 하자(프로젝트를 많이 이해한 뒤에)
 
 // app.use(logger('dev'));
 // app.use(bodyParser.json());
@@ -34,17 +34,21 @@ app.set('view engine', 'jade');
 // app.use(session({secret: '2C44774A-D649-4D44-9535-46E296EF984F'}));
 // app.use(bodyParser.urlencoded());
 // app.use(methodOverride());
-app.use(require('stylus').middleware(__dirname + '/public'));
+// app.use(require('stylus').middleware(path.join(__dirname, 'public')));
+// app.use(require('angular').middleware(__dirname + '/node_modules'));
+// app.use('/scripts', express.static(__dirname + '/node_modules'));
 app.use(express.static(path.join(__dirname, 'public'))); //정적파일 활용
+// app.use('/scripts', express.static(path.join(__dirname, 'node_modules')));
+//바로 위 미들웨어 동작하지 않는 것 같다;;
 
 // Development only
-// if ('development' === app.get('env')) {
+// if (app.get('env') === 'development') {
 //   app.use(errorHandler());
 // }
 
 // Pages and routes
-app.get('/', routes.index);
-app.get('/login', routes.test.login);
+// app.get('/', routes.index);
+// app.get('/login', routes.test.login);
 // app.get('/admin', authorize, routes.article.admin);
 // app.get('/post', authorize, routes.article.post);
 // app.post('/post', authorize, routes.article.postArticle);
@@ -57,9 +61,9 @@ app.get('/login', routes.test.login);
 // app.put('/api/articles/:id', routes.article.edit);
 // app.del('/api/articles/:id', routes.article.del);
 
-app.all('*', function(req, res) {
-  res.send(404);
-})
+// app.all('*', function(req, res) {
+//   res.send(404);
+// })
 
 // http.createServer(app).listen(app.get('port'), function(){
   // console.log('Express server listening on port ' + app.get('port'));
