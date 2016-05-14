4e85b2706cba759f078ed71b2210395ad4f9502e
brightlaw <kyeongmo2@gmail.com>
2016-05-14 20:19:33 +0900

    Update public

--- public/js/app.js
+++ public/js/app.js
@@ -18,6 +18,17 @@ app.config(['$routeProvider', function($routeProvider) {
 		});
 }]);
 
+app.factory('UserService', ['$resource', function($resource) {
+  return $resource('/users/:userId', {}, {
+		create: {
+			method: 'POST'
+		},
+    update: {
+      method: 'PUT'
+    }
+  });
+}]);
+
 app.controller('NewpageCtrl', function($scope) {
 	//컨트롤러 테스트용
 	$scope.x = 1;
@@ -26,8 +37,17 @@ app.controller('NewpageCtrl', function($scope) {
 	};
 })
 
-app.controller('SignCtrl', function($scope) {
+app.controller('SignCtrl',  ['$scope', '$routeParams', 'UserService', function($scope, $routeParams, user) {
 	//mongodb 연결 뒤 user 저장
 	//세션 유지는 어떻게 하나?
 	//세션에 따라 home.html 버튼 다르게 보이기(로그인/글쓰기 버튼 등등)
+
+	$scope.save = function() {
+		
+	}
 })
+
+function _handleError(response) {
+  // TODO: 여기서 뭔가를 수행한다. 대부분 오류 페이지로 리디렉트한다.
+  console.log('%c ' + response, 'color:red');
+}
