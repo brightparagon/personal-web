8b597ab54302392f1ceb958cd9c7a1a15eafe901
brightlaw <kyeongmo2@gmail.com>
2016-05-15 16:53:03 +0900

    Update public

--- public/js/app.js
+++ public/js/app.js
@@ -37,15 +37,21 @@ app.controller('NewpageCtrl', function($scope) {
 	};
 })
 
-app.controller('SignCtrl',  ['$scope', '$routeParams', 'UserService', function($scope, $routeParams, user) {
+app.controller('SignCtrl',  ['$scope', '$routeParams', 'UserService', function($scope, $routeParams, userService) {
 	//mongodb 연결 뒤 user 저장
 	//세션 유지는 어떻게 하나?
 	//세션에 따라 home.html 버튼 다르게 보이기(로그인/글쓰기 버튼 등등)
 
-	$scope.save = function() {
-		
-	}
-})
+	$scope.signup = function() {
+		var userInstance = {
+			email: $scope.email,
+			password: $scope.password
+		}
+		userService.create(userInstance, function() {
+			console.log('the user is made');
+		});
+	};
+}])
 
 function _handleError(response) {
   // TODO: 여기서 뭔가를 수행한다. 대부분 오류 페이지로 리디렉트한다.
