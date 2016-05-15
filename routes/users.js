cba4e4c8a38b9922bcd09bddafc42c2d8ac81d09
brightlaw <kyeongmo2@gmail.com>
2016-05-15 16:53:21 +0900

    Update routes(server)

--- routes/users.js
+++ routes/users.js
@@ -5,7 +5,7 @@ var router = express.Router();
 
 router.get('/users', function(req, res, next) {
   User.find().sort('name.last').exec(function(error, results) {
-    if (error) {
+    if(error) {
       return next(error);
     }
 
@@ -17,13 +17,13 @@ router.get('/users', function(req, res, next) {
 router.get('/users/:userId', function(req, res, next) {
   User.findOne({
     id: req.params.userId
-  }).exec(function (error, results) {
-    if (error) {
+  }).exec(function(error, results) {
+    if(error) {
       return next(error);
     }
 
     // 유효한 사용자를 찾지 못하면 404를 전송한다
-    if (!results) {
+    if(!results) {
       res.send(404);
     }
 
@@ -32,23 +32,30 @@ router.get('/users/:userId', function(req, res, next) {
   });
 });
 
-router.put('/users/:userId', function (req, res, next) {
+router.put('/users/:userId', function(req, res, next) {
   // 다음 행을 제거하면 몽구스가 오류를 던진다
   // 몽고DB ID를 갱신하려 시도하기 때문이다
   delete req.body._id;
 
   User.update({
     id: req.params.userId
-  }, req.body, function (err, numberAffected, response) {
-    if (err) {
-      return next(err);
+  }, req.body, function(error, numberAffected, response) {
+    if (error) {
+      return next(error);
     }
 
     res.send(200);
   });
 });
 
-// 몽구스 메서드 확인
-// create 메서드 작성
+router.post('/users/', function (req, res, next) {
+  User.create(req.params.user, function(error, results) {
+    if(error) {
+      return next(error);
+    }
+
+    res.json(results);
+  })
+});
 
 module.exports = router;
