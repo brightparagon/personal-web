135acc80c041d9a77a73fa1d46e03b549d26f76f
brightlaw <kyeongmo2@gmail.com>
2016-08-06 15:52:56 +0900

    create post schema

---
+++ mongodb/models/post.js
@@ -0,0 +1,32 @@
+var mongoose = require('mongoose');
+var Schema = mongoose.Schema;
+var postSchema = new Schema({
+  postedBy: {
+    type: Schema.Types.ObjectId,
+    required: true,
+    ref: 'User'
+  },
+  title: {
+    type: String,
+    required: true
+  },
+  isPrivate: {
+    type: Boolean,
+    required: true
+  },
+  content: {
+    type: String,
+    required: true
+  },
+  notekind: {
+    type: String,
+    required: true
+  },
+  updated: {
+    type: Date,
+    default: Date.now
+  },
+  tags: [String]
+});
+
+module.exports = mongoose.model('Post', postSchema);
