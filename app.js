69832655cc1cf6781708edcd38b3bd42663baa6d
brightlaw <kyeongmo2@gmail.com>
2017-01-16 15:06:11 +0900

    fix address in the footer

--- app.js
+++ app.js
@@ -1,6 +1,6 @@
 var express = require('express');
 var path = require('path');
-// var favicon = require('serve-favicon');
+var favicon = require('serve-favicon');
 var logger = require('morgan'); // leave log message
 var errorHandler = require('errorhandler');
 var cookieParser = require('cookie-parser');
